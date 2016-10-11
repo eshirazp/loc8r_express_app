acc9af14d73d0bf5e84535c7947eba340ce6537e
Elush Shirazpour <eshirazp@gmail.com>
2016-10-11 01:04:03 -0700

    Forgot to commit new routes and controllers filed so REST API

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+module.exports.locationsListByDistance = function(req, res) {
+  sendJSONresponse(res, 200, {"status": "success"});
+};
+
+module.exports.locationsCreate = function(req, res) {
+  sendJSONresponse(res, 200, {"status": "success"});
+};
+
+module.exports.locationsReadOne = function(req, res) {
+  sendJSONresponse(res, 200, {"status": "success"});
+};
+
+module.exports.locationsUpdateOne = function(req, res) {
+  sendJSONresponse(res, 200, {"status": "success"});
+};
+
+module.exports.locationsDeleteOne = function(req, res) {
+  sendJSONresponse(res, 200, {"status": "success"});
+};
