e74f75b7046196123c16be9d8a7102fa1e1df216
Shirazpour <shirel@ga.com>
2016-10-11 17:21:55 -0700

    Filling out controllers for REST API for locations and reviews. Controllers are only connected to the MongoDB through Mongoose

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -6,16 +6,129 @@ var sendJSONresponse = function(res, status, content) {
   res.json(content);
 };
 
+var theEarth = (function() {
+  var earthRadius = 6371; // km, miles is 3959
+
+  var getDistanceFromRads = function(rads) {
+    return parseFloat(rads * earthRadius);
+  };
+
+  var getRadsFromDistance = function(distance) {
+    return parseFloat(distance / earthRadius);
+  };
+
+  return {
+    getDistanceFromRads: getDistanceFromRads,
+    getRadsFromDistance: getRadsFromDistance
+  };
+})();
+
+/* GET list of locations */
 module.exports.locationsListByDistance = function(req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+  var maxDistance = parseFloat(req.query.maxDistance);
+  
+  var point = {
+    type: "Point",
+    coordinates: [lng, lat]
+  };
+
+  var geoOptions = {
+    spherical: true,
+    maxDistance: theEarth.getRadsFromDistance(maxDistance),
+    num: 10
+  };
+
+  if (!lng || !lat || !maxDistance) {
+    console.log('locationsListByDistance missing params');
+    sendJSONresponse(res, 404, {
+      "message": "lng, lat and maxDistance query parameters are all required"
+    });
+    return;
+  }
+
+  Loc.geoNear(point, geoOptions, function(err, results, stats) {
+    var locations;
+    console.log('Geo Results', results);
+    console.log('Geo stats', stats);
+    if (err) {
+      console.log('geoNear error:', err);
+      sendJSONresponse(res, 404, err);
+    } else {
+      locations = buildLocationList(req, res, results, stats);
+      sendJSONresponse(res, 200, locations);
+    }
+  });
+};
+
+var buildLocationList = function(req, res, results, stats) {
+  var locations = [];
+  results.forEach(function(doc) {
+    locations.push({
+      distance: theEarth.getDistanceFromRads(doc.dis),
+      name: doc.obj.name,
+      address: doc.obj.address,
+      rating: doc.obj.rating,
+      facilities: doc.obj.facilities,
+      _id: doc.obj._id
+    });
+  });
+  return locations;
+};
+
+/* GET a location by the id */
+module.exports.locationsReadOne = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid " + req.params.locationid + " not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
 };
 
 module.exports.locationsCreate = function(req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
-};
-
-module.exports.locationsReadOne = function(req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
+  Loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+    openingTimes: [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1
+    }, {
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2
+    }]
+  }, function(err, location) {
+    if(err) {
+      sendJSONresponse(res, 404, err);
+    } else {
+      sendJSONresponse(res, 201, location);
+    }
+  });
 };
 
 module.exports.locationsUpdateOne = function(req, res) {
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -11,8 +11,54 @@ module.exports.reviewsCreate = function(req, res) {
 };
 
 module.exports.reviewsReadOne = function(req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
+  console.log("Getting single review");
+  if (req.params && req.params.locationid && req.params.reviewid) {
+    Loc
+      .findById(req.params.locationid)
+      .select('name reviews')
+      .exec(
+        function(err, location) {
+          console.log(location);
+          var response, review;
+          if (!location) {
+            sendJSONresponse(res, 404, {
+              "message": "locationid " + req.params.locationid + " not found"
+            });
+            return;
+          } else if (err) {
+            sendJSONresponse(res, 400, err);
+            return;
+          }
+          if (location.reviews && location.reviews.length > 0) {
+            review = location.reviews.id(req.params.reviewid);
+            if (!review) {
+              sendJSONresponse(res, 404, {
+                "message": "reviewid " + req.params.reviewid + " not found"
+              });
+            } else {
+              response = {
+                location: {
+                  name: location.name,
+                  id: req.params.locationid
+                },
+                review: review
+              };
+              sendJSONresponse(res, 200, response);
+            }
+          } else {
+            sendJSONresponse(res, 404, {
+              "message": "No reviews found"
+            });
+          }
+        }
+    );
+  } else {
+    sendJSONresponse(res, 404, {
+      "message": "Not found, locationid and reviewid are both required"
+    });
+  }
 };
+
 module.exports.reviewsUpdateOne = function(req, res) {
   sendJSONresponse(res, 200, {"status": "success"});
 };
