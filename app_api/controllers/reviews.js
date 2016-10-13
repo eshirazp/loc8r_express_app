ba2adfec2ace9468eb27ed5f9feada8306d6d950
Shirazpour <shirel@ga.com>
2016-10-12 17:01:57 -0700

    Finished creating the back end with the API controllers. Moved on to connecting the front end with the back end REST API

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -135,8 +135,97 @@ module.exports.reviewsReadOne = function(req, res) {
 };
 
 module.exports.reviewsUpdateOne = function(req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
-};
+  if (!req.params.locationid || !req.params.reviewid) {
+    sendJSONresponse(res, 404, {
+      "message" : "Not found, locationid and reviewid are both required"
+    });
+    return;
+  }
+  Loc
+    .findById(req.params.locationid)
+    .select('reviews')
+    .exec(
+      function(err, location) {
+        var thisReview;
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message" : "locationid not found"
+          });
+          return;
+        } else if (err) {
+          sendJSONresponse(res, 400, err);
+          return;
+        }
+        if (location.reviews && location.reviews.length > 0) {
+          thisReview = location.reviews.id(req.params.reviewid);
+          if (!thisReview) {
+            sendJSONresponse(res, 404, {
+              "message" : "reviewid not found"
+            });
+          } else {
+            thisReview.author = req.body.author;
+            thisReview.rating = req.body.rating;
+            thisReview.reviewText = req.body.reviewText;
+            location.save(function(err, location) {
+              if(err) {
+                sendJSONresponse(res, 404, err);
+              } else {
+                updateAverageRating(location._id);
+                sendJSONresponse(res, 200, thisReview);
+              }
+            });
+          }
+        } else {
+          sendJSONresponse(res, 404, {
+            "message" : "No review to update"
+          });
+        }
+      }
+    );
+ };
+
 module.exports.reviewsDeleteOne = function(req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
+  if (!req.params.locationid || !req.params.reviewid) {
+    sendJSONresponse(res, 404, {
+      "message" : "Not found, locationid and reviewid are both required"
+    });
+    return;
+  }
+  Loc
+    .findById(req.params.locationid)
+    .select('reviews')
+    .exec(
+      function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message" : "locationid not found"
+          });
+          return;
+        } else if (err) {
+          sendJSONresponse(res, 400, err);
+          return;
+        }
+        if (location.reviews && location.reviews.length > 0) {
+          if (!location.reviews.id(req.params.reviewid)) {
+            sendJSONresponse(res, 404, {
+              "message" : "reviewid not found"
+            });
+          } else {
+            location.reviews.id(req.params.reviewid).remove();
+            location.save(function(err) {
+              if (err) {
+                sendJSONresponse(res, 404, err);
+              } else {
+                updateAverageRating(location._id);
+                sendJSONresponse(res, 204, null);
+              }
+            });
+          }
+        } else {
+          sendJSONresponse(res, 404, {
+            "message" : "no review to delete"
+          });
+        }
+      }
+  );
 };
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,89 +1,141 @@
+var request = require('request');
+var apiOptions = {
+  server: "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://pacific-refuge-13041.herokuapp.com";
+}
+
+var _isNumeric = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var _formatDistance = function (distance) {
+  var numDistance, unit;
+
+  if (distance && _isNumeric(distance)) {
+    if (distance > 1) {
+      numDistance = parseFloat(distance).toFixed(1);
+      unit = 'km';
+    } else {
+      numDistance = parseInt(distance * 1000,10);
+      unit = 'm';
+    }
+    return numDistance + unit;
+  } else {
+    return "?";
+  }
+};
+
+var renderHomepage = function(req, res, responseBody) {
+  var message;
+
+  if(!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No places found nearby";
+    }
+  }
+
+  res.render('locations-list', {
+      title: 'Loc8r - find a place to work with wifi',
+      pageHeader: {
+          title: 'Loc8r',
+          strapline: 'Find places to work with wifi near you!'
+      },
+      sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+      locations: responseBody,
+      message: message
+  });
+};
+
 /* GET 'home' page */
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', {
-        title: 'Loc8r - find a place to work with wifi',
-        pageHeader: {
-            title: 'Loc8r',
-            strapline: 'Find places to work with wifi near you!'
-        },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [{
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
-        }, {
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-        }, {
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
-        }]
-    });
+  var requestOptions , path;
+  path = '/api/locations';
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {},
+    qs: {
+      lng: -0.7992599,
+      lat: 51.378091,
+      maxDistance: 200000000000000
+    }
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var i, data;
+      data = body;
+
+      if (response.statusCode === 200 && data.length) {
+        for(i = 0; i < data.length; i++) {
+          data[i].distance = _formatDistance(data[i].distance);
+        }
+      }
+      renderHomepage(req, res, data);
+    }
+  );
 };
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', {
-        title: 'Starcups',
-        pageHeader: {
-            title: 'Starcups'
-        },
-        sidebar: {
-            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-        },
-        location: {
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            coords: {
-                lat: 51.455041,
-                lng: -0.9690884
-            },
-            openingTimes: [{
-                days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
-                closed: false
-            }, {
-                days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
-                closed: false
-            }, {
-                days: 'Sunday',
-                closed: true
-            }],
-            reviews: [{
-                author: 'Elush Shirazpour',
-                rating: 5,
-                timestamp: '16 July 2016',
-                reviewText: 'What a great place. I can\'t say enough good things about it.'
-            }, {
-                author: 'David Vigodnier',
-                rating: 3,
-                timestamp: '24 February 2015',
-                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-            }]
-        }
-    });
+  res.render('location-info', {
+      title: 'Starcups',
+      pageHeader: {
+          title: 'Starcups'
+      },
+      sidebar: {
+          context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+          callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+      },
+      location: {
+          name: 'Starcups',
+          address: '125 High Street, Reading, RG6 1PS',
+          rating: 3,
+          facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+          coords: {
+              lat: 51.455041,
+              lng: -0.9690884
+          },
+          openingTimes: [{
+              days: 'Monday - Friday',
+              opening: '7:00am',
+              closing: '7:00pm',
+              closed: false
+          }, {
+              days: 'Saturday',
+              opening: '8:00am',
+              closing: '5:00pm',
+              closed: false
+          }, {
+              days: 'Sunday',
+              closed: true
+          }],
+          reviews: [{
+              author: 'Elush Shirazpour',
+              rating: 5,
+              timestamp: '16 July 2016',
+              reviewText: 'What a great place. I can\'t say enough good things about it.'
+          }, {
+              author: 'David Vigodnier',
+              rating: 3,
+              timestamp: '24 February 2015',
+              reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+          }]
+      }
+  });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', {
-        title: 'Review Starcups on Loc8r',
-        pageHeader: {
-            title: 'Review Starcups'
-        }
-    });
+  res.render('location-review-form', {
+      title: 'Review Starcups on Loc8r',
+      pageHeader: {
+          title: 'Review Starcups'
+      }
+  });
 };
